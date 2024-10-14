1080da03205edf985bed800ceba6432fecc9b92b
Tom <98466837+TomPARIS1@users.noreply.github.com>
2024-10-14 19:23:04 +0200

    Finish component Project + fix navbar

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Spotlight } from './ui/Spotlight'
+import { Spotlight } from './ui/spotlight'
 import { TextGenerateEffect } from './ui/text-generate-effect'
 import MagicButton from './ui/magic-button'
 import { FaLocationArrow } from 'react-icons/fa6'
