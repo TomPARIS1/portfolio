28df8b669a4f79d1b92de71f898c26f43a64f7c2
Tom <98466837+TomPARIS1@users.noreply.github.com>
2024-10-19 12:59:40 +0200

    Form activation

--- components/ui/contact-form.tsx
+++ components/ui/contact-form.tsx
@@ -5,54 +5,73 @@ import { Input } from "./input";
 import { cn } from "@/lib/utils";
 
 export function ContactForm() {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("Form submitted");
-  };
-  return (
-    <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-neutral-900">
+    async function handleSubmit(event: any) {
+        event.preventDefault();
+        const formData = new FormData(event.target);
 
-      <form className="my-4" onSubmit={handleSubmit}>
-        <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
-          <LabelInputContainer>
-            <Label htmlFor="firstname">Prénom</Label>
-            <Input id="firstname" placeholder="Tyler" type="text" />
-          </LabelInputContainer>
-          <LabelInputContainer>
-            <Label htmlFor="lastname">Nom</Label>
-            <Input id="lastname" placeholder="Durden" type="text" />
-          </LabelInputContainer>
+        formData.append("access_key", "923fb011-75e1-4e4d-9deb-efd84dd11f23");
+
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json"
+            },
+            body: json
+        });
+        const result = await response.json();
+        if (result.success) {
+            console.log(result);
+        }
+    }
+
+    return (
+        <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-neutral-900">
+
+        <form className="my-4" onSubmit={handleSubmit}>
+            <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+            <LabelInputContainer>
+                <Label htmlFor="firstname">Prénom</Label>
+                <Input id="firstname" placeholder="Tyler" type="text" name="first_name" required/>
+            </LabelInputContainer>
+            <LabelInputContainer>
+                <Label htmlFor="lastname">Nom</Label>
+                <Input id="lastname" placeholder="Durden" type="text" name="last_name" required/>
+            </LabelInputContainer>
+            </div>
+            <LabelInputContainer className="mb-4">
+            <Label htmlFor="email">E-mail</Label>
+            <Input id="email" placeholder="firstrule@gmail.com" type="email" name="email" required/>
+            </LabelInputContainer>
+            <LabelInputContainer className="mb-4">
+                <Label htmlFor="subject">Sujet</Label>
+                <Input id="subject" placeholder="Sujet..." type="text" name="subject" required/>
+            </LabelInputContainer>
+            <LabelInputContainer className="mb-8">
+                <Label htmlFor="subject">Message</Label>
+                <textarea id="subject" placeholder="Message..." name="message" className="flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white shadow-input rounded-md px-3 py-2 text-sm  file:border-0 file:bg-transparent 
+            file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 
+            focus-visible:outline-none focus-visible:ring-[2px]  focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600
+            disabled:cursor-not-allowed disabled:opacity-50
+            dark:shadow-[0px_0px_1px_1px_var(--neutral-700)]
+            group-hover/input:shadow-none transition duration-400
+            min-h-[80px]" required/>
+            </LabelInputContainer>
+
+            <button
+            className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+            type="submit"
+            >
+            Envoyer &rarr;
+            <BottomGradient />
+            </button>
+
+        </form>
         </div>
-        <LabelInputContainer className="mb-4">
-          <Label htmlFor="email">E-mail</Label>
-          <Input id="email" placeholder="firstrule@gmail.com" type="email" />
-        </LabelInputContainer>
-        <LabelInputContainer className="mb-4">
-            <Label htmlFor="subject">Sujet</Label>
-            <Input id="subject" placeholder="Sujet..." type="text" />
-        </LabelInputContainer>
-        <LabelInputContainer className="mb-8">
-            <Label htmlFor="subject">Message</Label>
-            <textarea id="subject" placeholder="Message..." className="flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white shadow-input rounded-md px-3 py-2 text-sm  file:border-0 file:bg-transparent 
-          file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 
-          focus-visible:outline-none focus-visible:ring-[2px]  focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600
-           disabled:cursor-not-allowed disabled:opacity-50
-           dark:shadow-[0px_0px_1px_1px_var(--neutral-700)]
-           group-hover/input:shadow-none transition duration-400
-           min-h-[80px]"/>
-        </LabelInputContainer>
-
-        <button
-          className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
-          type="submit"
-        >
-          Envoyer &rarr;
-          <BottomGradient />
-        </button>
-
-      </form>
-    </div>
-  );
+    );
 }
 
 const BottomGradient = () => {
--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -4,7 +4,6 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 import { useMotionTemplate, useMotionValue, motion } from "framer-motion";
 
-// tslint:disable-next-line:no-empty-interface
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
