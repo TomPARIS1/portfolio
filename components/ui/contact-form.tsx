49d5a79f2e5cb3a4bd52a4056c506b3e995483a1
Tom <98466837+TomPARIS1@users.noreply.github.com>
2024-10-22 00:48:09 +0200

    Add captcha verification form

--- components/ui/contact-form.tsx
+++ components/ui/contact-form.tsx
@@ -3,10 +3,17 @@ import React, { useState } from "react";
 import { Label } from "./label";
 import { Input } from "./input";
 import { cn } from "@/lib/utils";
+import HCaptcha from '@hcaptcha/react-hcaptcha';
+import { useForm } from "react-hook-form";
 
 export function ContactForm() {
     const [formSubmitted, setFormSubmitted] = useState(false);
     const [errorMessage, setErrorMessage] = useState("");
+    const { setValue } = useForm();
+
+    const onHCaptchaChange = (token: any) => {
+      setValue("h-captcha-response", token);
+    };
 
     async function handleSubmit(event: any) {
         event.preventDefault();
@@ -76,22 +83,25 @@ export function ContactForm() {
             <LabelInputContainer className="mb-8">
                 <Label htmlFor="subject">Message</Label>
                 <textarea id="subject" placeholder="Message..." name="message" className="flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white shadow-input rounded-md px-3 py-2 text-sm  file:border-0 file:bg-transparent 
-            file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 
-            focus-visible:outline-none focus-visible:ring-[2px]  focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600
-            disabled:cursor-not-allowed disabled:opacity-50
-            dark:shadow-[0px_0px_1px_1px_var(--neutral-700)]
-            group-hover/input:shadow-none transition duration-400
-            min-h-[80px]" required/>
+                  file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-700)] group-hover/input:shadow-none transition duration-400 min-h-[80px]"  required
+                />
             </LabelInputContainer>
 
             <button
-              className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+              className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset] mb-8"
               type="submit"
             >
               Envoyer &rarr;
-            <BottomGradient />
+              <BottomGradient />
             </button>
 
+            <div className="flex justify-center items-center">
+              <HCaptcha
+                sitekey="50b2fe65-b00b-4b9e-ad62-3ba471098be2"
+                reCaptchaCompat={false}
+                onVerify={onHCaptchaChange}
+              />
+            </div>
         </form>
       </div>
     );
