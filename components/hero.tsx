d9c5993f3a811e7c85e8eac775ea43c8a1044ffb
Tom <98466837+TomPARIS1@users.noreply.github.com>
2025-10-09 09:46:42 +0200

    Edited hero background + text

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,80 +1,79 @@
 "use client"
 
 import React from "react";
-import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
 import { FaDownload } from "react-icons/fa6";
 import MagicButton from "./ui/magic-button";
+import { Spotlight } from "./ui/spotlight";
+import { BackgroundGradientAnimation } from "./ui/background-gradient-animation";
 
 const Hero = () => {
   return (
     <section className="lg:py-16 h-screen w-full flex justify-center items-center">
         <div className="h-screen w-full dark:bg-neutral-900 bg-white flex items-center justify-center absolute top-0 left-0 blur-xl">
-            
+            <BackgroundGradientAnimation/>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-12">
-        <motion.div
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 0.5 }}
-            className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
-        >
-            <h1 className="text-neutral-400 mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
-            <span className="text-white bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
-                Bonjour, je suis
-            </span>
-            <br></br>
-            <TypeAnimation
-                sequence={[
-                "Tom",
-                1000,
-                "Développeur passionné",
-                1000,
-                ]}
-                wrapper="span"
-                speed={50}
-                repeat={Infinity}
+        <div>
+            <Spotlight
+            className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+            fill="white"
             />
-            </h1>
-            <p className="text-white text-base sm:text-lg mb-6 lg:text-xl">
-            Transformer vos idées en expériences captivantes !
-            <br/>
-            Développeur web freelance React / Next.js — Disponible pour missions à distance.
-            </p>
-            <div className="flex gap-4">
-                <a href="#contact">
-                    <MagicButton
-                        title="Me contacter"
-                        icon=""
-                        position=""
-                    />
-                </a>
-                <a href="/CV_Tom_PARIS.pdf" download>
-                    <MagicButton
-                        title="Télécharger mon CV"
-                        icon={<FaDownload />}
-                        position="right"
-                    />
-                </a>
-            </div>
-        </motion.div>
-        <motion.div
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 0.5 }}
-            className="col-span-4 place-self-center mt-4 lg:mt-0"
-        >
-            <div className="rounded-full bg-neutral-950 w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-            <Image
-                src="/logo-hero.png"
-                alt="Head Logo"
-                className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                width={300}
-                height={300}
+            <Spotlight
+            className="h-[80vh] w-[50vw] top-10 left-full"
+            fill="gray"
             />
+            <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="black" />
+        </div>
+
+
+
+        <div className="flex justify-center relative my-20 z-10">
+            <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+                <motion.div
+                    initial={{ opacity: 0, scale: 0.5 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.5 }}
+                    className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+                >
+                    <h1 className="text-neutral-400 mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold text-center">
+                    <span className="text-white bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
+                        Bonjour, je suis
+                    </span>
+                    <br></br>
+                    <TypeAnimation
+                        sequence={[
+                        "Tom",
+                        1000,
+                        "Développeur web",
+                        1000,
+                        ]}
+                        wrapper="span"
+                        speed={50}
+                        repeat={Infinity}
+                    />
+                    </h1>
+                    <p className="text-white text-base sm:text-lg mb-6 lg:text-xl text-center">
+                    Transformer vos idées en expériences captivantes à l'aide de technologies modernes !
+                    </p>
+                    <div className="flex gap-4 justify-center items-center">
+                        <a href="#contact">
+                            <MagicButton
+                                title="Me contacter"
+                                icon=""
+                                position=""
+                            />
+                        </a>
+                        <a href="/CV_Tom_PARIS.pdf" download>
+                            <MagicButton
+                                title="Télécharger mon CV"
+                                icon={<FaDownload />}
+                                position="right"
+                            />
+                        </a>
+                    </div>
+                </motion.div>
             </div>
-        </motion.div>
         </div>
     </section>
   )
--- components/ui/background-gradient-animation.tsx
+++ components/ui/background-gradient-animation.tsx
@@ -3,14 +3,14 @@ import { cn } from "@/lib/utils";
 import { useEffect, useRef, useState } from "react";
 
 export const BackgroundGradientAnimation = ({
-  gradientBackgroundStart = "rgb(108, 0, 162)",
-  gradientBackgroundEnd = "rgb(0, 17, 82)",
-  firstColor = "18, 113, 255",
-  secondColor = "221, 74, 255",
-  thirdColor = "100, 220, 255",
-  fourthColor = "200, 50, 50",
-  fifthColor = "180, 180, 50",
-  pointerColor = "140, 100, 255",
+  gradientBackgroundStart = "rgb(71, 71, 71)",
+  gradientBackgroundEnd = "rgb(0, 0, 0)",
+  firstColor = "100, 100, 100",
+  secondColor = "10, 10, 10",
+  thirdColor = "70, 70, 70",
+  fourthColor = "30, 30, 30",
+  fifthColor = "120, 120, 120",
+  pointerColor = "140, 140, 140",
   size = "80%",
   blendingValue = "hard-light",
   children,
