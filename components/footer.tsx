1cd66fdab7aacf92fb89069970d626d94f99e03c
Tom <98466837+TomPARIS1@users.noreply.github.com>
2026-01-02 15:25:42 +0100

    Fixed form responsive border & footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image'
 const Footer = () => {
     return (
         <footer className="w-full pt-20 pb-10 mb-[100px] md:mb-5" id="contact">
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center ">
 
                 <h2 className="heading lg:max-w-[45vw]">
                     Envie de <span className="text-neutral-400">donner vie</span> à vos idées ?
@@ -17,9 +17,9 @@ const Footer = () => {
             </div>
 
             <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
-                <p className="md:text-base text-sm md:font-normal font-light sm:mb-2">Copyright © {new Date().getFullYear()} Tom Paris</p>
+                <p className="md:text-base text-sm md:font-normal font-light mb-6 md:mb-0">Copyright © {new Date().getFullYear()} Tom Paris</p>
 
-                <div className="flex items-center md:gap-3 gap-6">
+                <div className="flex items-center md:gap-3 gap-6 ">
                     {socialMedia.map((profile) => (
                         <div key={profile.id} className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-neutral-900 rounded-lg border border-black-300">
                             <a href={profile.link}><Image src={profile.img} alt={"Logo" + profile.id} width={20} height={20} /></a>
